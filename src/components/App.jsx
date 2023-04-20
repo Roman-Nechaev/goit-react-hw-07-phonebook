9795a743d65d95cdcd83cd25cd44e8fbb25f7236
Roman-Nechaev <nrchaevroman@gmail.com>
2023-04-20 23:38:07 +0300

    settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+import { Container, Title, TitleContacts } from './App.styled';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Title>PhoneBook</Title>
+      <ContactForm />
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter />
+      <ContactsList />
+    </Container>
   );
-};
+}
+
+export default App;
